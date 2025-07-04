57fde8dfb8100732658fd16472ab2d9dc174b1ca
amaucher <andreas-maucher@gmx.de>
2025-07-04 21:24:52 +0200

    setup directories and knowledge base

---
+++ knowledge.cpp
@@ -0,0 +1,14 @@
+/*
+DOCKER COMPOSE
+
+tool to define and run multi-container applications
+all services (containers), networks, volumes are described in the .yml file
+"docker compose up" to start the infrastructure
+
+Image: Blueprint for a container
+Container: Running instance of an image
+Docker Volumes: special directories on the host (if a container is deleted, the data in the volume remains)
+
+-> with compose images are run and built according to .yml file
+
+*/
