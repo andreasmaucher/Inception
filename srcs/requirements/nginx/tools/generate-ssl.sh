df38869f71e4b783dbfb452174a211dfefdf5d61
amaucher <andreas-maucher@gmx.de>
2025-09-26 23:58:45 +0200

    adjusted generate-ssl for nginx

--- srcs/requirements/nginx/tools/generate-ssl.sh
+++ srcs/requirements/nginx/tools/generate-ssl.sh
@@ -4,10 +4,4 @@
 mkdir -p /etc/nginx/ssl
 
 # Generate self-signed certificate so WordPress can use HTTPS
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    # where private key is saved
-    -keyout /etc/nginx/ssl/key.pem \
-    # where certificate is saved
-    -out /etc/nginx/ssl/cert.pem \
-    # certificate details
-    -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}"
